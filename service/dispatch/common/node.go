e9153ef57d571915d7046a8e845221cfb5c8eb66
neverCase <1024769485@qq.com>
2019-09-23 18:58:01 +0800

    service handle

--- service/dispatch/common/node.go
+++ service/dispatch/common/node.go
@@ -16,6 +16,7 @@ type Node struct {
 	allocatedId int32
 	heartTime   time.Time
 	status      int
+	count       int
 }
 
 type NodeHub struct {
@@ -34,20 +35,81 @@ func (s *Service) NewNodeHub() *NodeHub {
 			allocatedId: 0,
 			heartTime:   time.Now(),
 			status:      NodeIdle,
+			count:       0,
 		}
 		nh.nodes[addr] = n
 	}
+	go s.loop()
 	return nh
 }
 
-func (s *Service) handleGetAllocatedNode() {
-	
+func (s *Service) loop() {
+	tick := time.NewTicker(time.Second * 1)
+	defer tick.Stop()
+	for {
+		select {
+		case <-s.ctx.Done():
+			return
+		case <-tick.C:
+			s.nodeHub.mu.Lock()
+			for _, v := range s.nodeHub.nodes {
+				if v.status == NodeIdle {
+					continue
+				}
+				if t, ok := s.hub.connections[v.allocatedId]; ok {
+					if t.status == ConnClosed {
+						v.allocatedId = 0
+						v.status = NodeIdle
+					}
+				}
+			}
+			s.nodeHub.mu.Unlock()
+		}
+	}
 }
 
-func (s *Service) handlePingNode(addr string) {
-
+func (s *Service) handleGetAllocatedNode(id int32) {
+	s.nodeHub.mu.Lock()
+	defer s.nodeHub.mu.Unlock()
+	var b []string
+	for k, v := range s.nodeHub.nodes {
+		if v.status == NodeInUse {
+			continue
+		}
+		b = append(b, k)
+	}
+	for i := 0; i < len(b); i++ {
+		for j := 0; j < len(b)-1-i; j++ {
+			if s.nodeHub.nodes[b[j]].count >= s.nodeHub.nodes[b[j+1]].count {
+				b[j], b[j+1] = b[j+1], b[j]
+			}
+		}
+	}
+	if len(b) > 0 {
+		s.nodeHub.nodes[b[0]].allocatedId = id
+		s.nodeHub.nodes[b[0]].status = NodeInUse
+		s.nodeHub.nodes[b[0]].heartTime = time.Now()
+	}
 }
 
-func (s *Service) handleCompleteNode(addr string) {
+func (s *Service) handlePingNode(id int32, addr string) {
+	s.nodeHub.mu.Lock()
+	defer s.nodeHub.mu.Unlock()
+	if t, ok := s.nodeHub.nodes[addr]; ok {
+		if t.allocatedId == id {
+			t.heartTime = time.Now()
+		}
+	}
+}
 
+func (s *Service) handleCompleteNode(id int32, addr string) {
+	s.nodeHub.mu.Lock()
+	defer s.nodeHub.mu.Unlock()
+	if t, ok := s.nodeHub.nodes[addr]; ok {
+		if t.allocatedId == id {
+			t.allocatedId = 0
+			t.status = NodeIdle
+			t.count++
+		}
+	}
 }
