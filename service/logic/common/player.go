0d83813ee0a1adbba6dc2f2f18f11505d86062ec
neverCase <1024769485@qq.com>
2019-10-24 15:49:14 +0800

    remove tmp log

--- service/logic/common/player.go
+++ service/logic/common/player.go
@@ -27,11 +27,8 @@ func (w *Worker) PullPlayerOne(pid int, clear bool) (meet map[int]int, min int, 
 func (w *Worker) UpdatePlayerValue(p *Player, min int) {
 	if min != 0 {
 		min = w.TransferMinTime(min, p.Delay)
-		log.Printf("UpdatePlayerValue TransferMinTime min:%d", min)
 	}
-	log.Printf("UpdatePlayerValue 111111 min:%d", min)
 	if p.Value != min {
-		log.Printf("UpdatePlayerValue 2222222 min:%d", min)
 		p.Value = min
 		w.listNodes.AppendOrModify(p)
 	}
@@ -42,19 +39,14 @@ func (w *Worker) UpdatePlayerSettings(p *Player) (update bool, err error) {
 		return true, err
 	} else {
 		if p.Value == 0 {
-			log.Println(111111111)
 			p.Delay = delay
 		} else {
-			log.Println(2222222)
 			if p.Delay != delay {
-				log.Println(333333)
 				if p.Delay == DelayDefault {
-					log.Println(44444)
 					p.Delay = delay
 					p.Value = w.TransferMinTime(p.Value, delay)
 					return false, nil
 				} else {
-					log.Println(5555555)
 					p.Delay = delay
 					return true, nil
 				}
@@ -99,26 +91,21 @@ func (w *Worker) RefreshOne(str string, update bool) (err error) {
 	}
 	log.Println("RefreshOne p:", p)
 	if len(tmp) > 1 {
-		log.Println("RefreshOne UpdatePlayerSettings 111111")
 		if up, err = w.UpdatePlayerSettings(p); err != nil {
 			return err
 		}
 		if up == false {
-			log.Println("RefreshOne UpdatePlayerValue 222222")
 			w.UpdatePlayerValue(p, p.Value)
 			return nil
 		}
 	}
-	log.Println("RefreshOne start lock 1111")
 	p.mu.Lock()
 	defer func() {
-		log.Println("RefreshOne close lock")
 		p.mu.Unlock()
 	}()
 	if _, min, err := w.PullPlayerOne(pid, false); err != nil {
 		return err
 	} else {
-		log.Println("RefreshOne UpdatePlayerValue 3333333 PullPlayerOne-min:", min)
 		w.UpdatePlayerValue(p, min)
 		return nil
 	}
